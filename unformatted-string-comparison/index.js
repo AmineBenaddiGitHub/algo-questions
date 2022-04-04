15bbb983148fb432acd84e474ffa3424d6b6f78e
Amine Benaddi <abenaddi@salesforce.com>
2022-04-04 14:01:40 +0000

    unformatted strings comparison

---
+++ unformatted-string-comparison/index.js
@@ -0,0 +1,27 @@
+const formatter = (s, ptr = 0) => {
+  if (ptr > s.length) return s;
+  if (s[ptr] !== "#" && s[ptr] !== "%") return formatter(s, ptr + 1);
+  if (s[ptr] === "#") {
+    const part1 = ptr === 0 ? "" : s.slice(0, ptr - 1);
+    const part2 = s.slice(ptr + 1);
+    return formatter(part1 + part2, Math.max(ptr - 1, 0));
+  }
+  if (s[ptr] === "%") {
+    const idx = s
+      .slice(ptr)
+      .split("")
+      .findIndex((e) => e !== "#" && e !== "%");
+    const part1 = ptr === 0 ? "" : s.slice(0, ptr);
+    const part2 = idx === -1 ? slice(ptr + 1) : s.slice(ptr + 1, ptr + idx);
+    const part3 = idx === -1 ? "" : s.slice(ptr + idx + 1);
+    return formatter(part1 + part2 + part3, ptr);
+  }
+};
+
+const equalWithDeletions = (m, n) => formatter(m) === formatter(n);
+
+// console.log(formatter("a##x", 0));
+console.log(equalWithDeletions("a##x", "#a#x"));
+console.log(
+  equalWithDeletions("fi##f%%%th %%year #time###", "fifth year time")
+);
