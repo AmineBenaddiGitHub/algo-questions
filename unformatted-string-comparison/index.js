69e49e64c89eaad5e077e67281c42aa4aeb216cc
Amine Benaddi <abenaddi@salesforce.com>
2022-04-04 14:05:33 +0000

    unformatted strings comparison

--- unformatted-string-comparison/index.js
+++ unformatted-string-comparison/index.js
@@ -1,3 +1,16 @@
+/**
+Given two strings n and m, return true if they are equal when both are entered into text editors.
+But: a # means a backspace character (deleting backwards), and a % means a delete character (deleting forwards).
+
+Example:
+
+> equalWithDeletions("a##x", "#a#x")
+> true      // both strings become "x"
+
+> equalWithDeletions("fi##f%%%th %%year #time###", "fifth year time")
+> false     // the first string becomes "fart"
+ */
+
 const formatter = (s, ptr = 0) => {
   if (ptr > s.length) return s;
   if (s[ptr] !== "#" && s[ptr] !== "%") return formatter(s, ptr + 1);
