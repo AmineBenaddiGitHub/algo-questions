62a4904870708384e47c042229ff9ffaab00e4c0
Amine Benaddi <abenaddi@salesforce.com>
2022-04-27 10:06:02 +0000

    merge intervals

--- merge-intervals/index.js
+++ merge-intervals/index.js
@@ -1,3 +1,13 @@
+/**
+Given an array of intervals, merge the overlapping intervals, and return an array of the resulting intervals.
+Example:
+$ mergeIntervals([[1,4],[2,6],[8,10],[15,20]])
+$ [[1,6],[8,10],[15,20]]
+
+$ mergeIntervals([[1,2],[2,7]])
+$ [[1,7]]
+*/
+
 const mergeIntervals = (arraysArray, cpt = 0) => {
   let shouldMerge = false;
   let idx = 0;
