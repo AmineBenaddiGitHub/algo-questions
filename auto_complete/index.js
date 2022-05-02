26b1d58bec55436bdcf278cc33c0f4d8a2cdaee8
Amine Benaddi <abenaddi@salesforce.com>
2022-05-02 12:29:09 +0000

    algo complete

--- auto_complete/index.js
+++ auto_complete/index.js
@@ -1,3 +1,16 @@
+/**
+let dict = ['apple', 'banana', 'cranberry', 'strawberry']
+
+$ simpleAutocomplete('app')
+$ ['apple']
+
+$ simpleAutocomplete('berry')
+$ ['cranberry', 'strawberry']
+
+$ simpleAutocomplete('fart')
+$ []
+*/
+
 const dict = ["apple", "banana", "cranberry", "strawberry"];
 
 const simpleAutocomplete = (string) => dict.filter((e) => e.includes(string));
