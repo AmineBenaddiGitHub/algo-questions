be7ab73f0e54ed27959f1e9bcbb7faaba984bf0e
Amine Benaddi <abenaddi@salesforce.com>
2022-05-16 09:57:03 +0100

    maximizedArray

--- maximizedArray/index.js
+++ maximizedArray/index.js
@@ -1,3 +1,17 @@
+/**
+This week’s question:
+Given two integer arrays of size n, return a new array of size n such that n consists of only unique
+elements and the sum of all its elements is maximum.
+
+Example:
+
+let arr1 = [7, 4, 10, 0, 1]
+let arr2 = [9, 7, 2, 3, 6]
+
+$ maximizedArray(arr1, arr2)
+$ [9, 7, 6, 4, 10]
+*/
+
 const arr1 = [7, 4, 10, 0, 1];
 const arr2 = [9, 7, 2, 3, 6];
 
