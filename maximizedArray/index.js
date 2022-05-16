8b40129c7f22b068756bcbe7654da06a48e822ae
Amine Benaddi <abenaddi@salesforce.com>
2022-05-17 00:07:51 +0100

    maximizedArray

--- maximizedArray/index.js
+++ maximizedArray/index.js
@@ -16,10 +16,10 @@ const arr1 = [7, 4, 10, 0, 1];
 const arr2 = [9, 7, 2, 3, 6];
 
 const maximizedArray = (arr1, arr2) => {
-  const totArray = [...arr1, ...arr2].sort((a, b) => b - a);
-  let lenArray = arr1.length - 1,
-    idx = 1,
+  const totArray = [...arr1, ...arr2].sort((a, b) => b - a),
     resArray = [totArray[0]];
+  let lenArray = arr1.length - 1,
+    idx = 1;
   while (lenArray) {
     if (totArray[idx] !== totArray[idx - 1]) {
       resArray.push(totArray[idx]);
