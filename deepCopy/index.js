96d93735fd613c95cc00526dd7f1c8cfe9997dee
Amine Benaddi <abenaddi@salesforce.com>
2022-07-11 10:52:57 +0100

    deepcopy linked list + random

---
+++ deepCopy/index.js
@@ -0,0 +1,52 @@
+function Node(val, next, random) {
+  this.val = val;
+  this.next = next;
+  this.random = random;
+}
+
+const head = new Node(0, null, null);
+const elm1 = new Node(1, null, null);
+head.next = elm1;
+const elm2 = new Node(2, null, null);
+elm1.next = elm2;
+const elm3 = new Node(3, null, null);
+elm2.next = elm3;
+const elm4 = new Node(4, null, null);
+elm3.next = elm4;
+const elm5 = new Node(5, null, null);
+elm4.next = elm5;
+
+head.random = elm3;
+elm1.random = head;
+elm2.random = null;
+elm3.random = elm5;
+elm4.random = head;
+elm5.random = elm2;
+
+function deepCopy(head) {
+  const newHead = new Node(head.val, null, null);
+  const stack = [newHead];
+  let ptr = head;
+  let ptrC = newHead;
+  while (ptr.next) {
+    const elm = new Node(ptr.next.val, null, null);
+    ptrC.next = elm;
+    stack.push(elm);
+    ptr = ptr.next;
+    ptrC = ptrC.next;
+  }
+  ptr = head;
+  ptrC = newHead;
+  while (ptr) {
+    const rand = ptr.random;
+    if (rand) {
+      const randC = stack.find((e) => e.val === rand.val);
+      ptrC.random = randC;
+    }
+    ptr = ptr.next;
+    ptrC = ptrC.next;
+  }
+  return newHead;
+}
+
+const newHead = deepCopy(head);
