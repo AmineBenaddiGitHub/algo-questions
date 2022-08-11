e8f78c5f426b0aaa98f3413437175e79d9b32266
Amine Benaddi <abenaddi@salesforce.com>
2022-08-11 12:21:11 +0100

    swap pairs

--- swap-pairs/index.js
+++ swap-pairs/index.js
@@ -11,6 +11,8 @@ const swapPairs = (array) =>
           : array[idx - 1]
       );
 
+console.log(swapPairs([1, 2, 3, 4, 5, 6]));
+console.log(swapPairs([1, 2, 3, 4, 5]));
 console.log(swapPairs([1, 2, 3, 4]));
 console.log(swapPairs([1, 2, 3]));
 console.log(swapPairs([1, 2]));
