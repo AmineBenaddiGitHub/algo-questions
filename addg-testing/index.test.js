abe2ae5900a3e1ff9483012410a4de5cff285875
Amine Benaddi <abenaddi@salesforce.com>
2022-08-29 12:00:09 +0100

    addg testing

--- addg-testing/index.test.js
+++ addg-testing/index.test.js
@@ -19,8 +19,12 @@ describe("testing addg high order function", () => {
     expect(addg()).toBeUndefined();
   });
   it("returns the same result as input when 1 param is called", () => {
-    const nb = 10;
-    expect(addg(nb)()).toBe(nb);
+    const arr = Array.from({ length: 10 }, (_) =>
+      Math.floor(Math.random() * 100)
+    );
+    arr.forEach((e) => {
+      expect(addg(e)()).toBe(e);
+    });
   });
   it("returns a function when no undefined call is done at the end", () => {
     expect(typeof addg(1)).toBe("function");
