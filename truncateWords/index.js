98bf954e62d30d1a03b390980759db225225bb53
Amine Benaddi <abenaddi@salesforce.com>
2022-10-11 09:08:21 +0100

    truncate words

--- truncateWords/index.js
+++ truncateWords/index.js
@@ -1,3 +1,17 @@
+/*
+Write a function that truncates words in a string to length n.
+
+Example:
+
+let n = 3
+
+> truncate('never gonna give you up', n)
+> 'nev gon giv you up'
+
+> truncate('*hello* darkness, my ~old_friend', n)
+> '*hel* dar, my ~old_fri'
+*/
+
 function truncate(s, n) {
   let arr = s.split(""),
     acc = 0;
