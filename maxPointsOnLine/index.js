b7ebc23d1747ab1a368d603c5ece15e3a629c13f
Amine Benaddi <Benaddi.Amine@bcg.com>
2023-04-20 03:06:26 +0000

    max points on line

---
+++ maxPointsOnLine/index.js
@@ -0,0 +1,32 @@
+function maxPointsOnLine(array) {
+  let maxPoints = 0;
+  array.forEach((element, idx) => {
+    const vectors = [];
+    array.forEach((node, jdx) => {
+      if (idx < jdx) {
+        vectors.push([node[0] - element[0], node[1] - element[1]]);
+      }
+    });
+    vectors.forEach((vector1) => {
+      let localMaxPointsOnLine = 0;
+      vectors.forEach((vector2) => {
+        if (vector1[0] * vector2[1] === vector1[1] * vector2[0]) {
+          localMaxPointsOnLine++;
+        }
+      });
+      if (localMaxPointsOnLine > maxPoints) maxPoints = localMaxPointsOnLine;
+    });
+  });
+  return maxPoints + 1;
+}
+
+console.log(
+  maxPointsOnLine([
+    [1, 1],
+    [3, 2],
+    [5, 3],
+    [4, 1],
+    [2, 3],
+    [1, 4],
+  ])
+);
