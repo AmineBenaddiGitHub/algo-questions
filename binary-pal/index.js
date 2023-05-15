283b956be1e9bb3f39ac50e891212bd6bf5567fc
Amine Benaddi <Benaddi.Amine@bcg.com>
2023-05-15 09:04:35 +0100

    binaryPal

---
+++ binary-pal/index.js
@@ -0,0 +1,16 @@
+function binaryPal(n) {
+  const bin = n.toString(2);
+  const len = bin.length;
+  return bin
+    .split("")
+    .reduce(
+      (acc, val, idx) =>
+        idx < Math.round(len / 2) ? acc && val === bin[len - idx - 1] : acc,
+      true
+    );
+}
+
+console.log(binaryPal(5));
+console.log(binaryPal(7));
+console.log(binaryPal(15));
+console.log(binaryPal(10));
