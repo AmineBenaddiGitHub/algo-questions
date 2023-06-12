66d4550e261e9fc4056cbf18b2c93177d3878c95
AmineBenaddiGitHub <36155069+AmineBenaddiGitHub@users.noreply.github.com>
2023-06-12 09:13:32 +0100

    trimArray

---
+++ trim_array/index.js
@@ -0,0 +1,10 @@
+function trimArray(arr, start, end) {
+  return arr.slice(start, arr.length - end);
+}
+
+console.log(trimArray([1, 2, 3, 4, 5, 6], 2, 1));
+console.log(trimArray([6, 2, 4, 3, 7, 1, 3], 5, 0));
+console.log(trimArray([1, 7], 0, 0));
+console.log(trimArray([1, 7], 10, 90));
+console.log(trimArray([1, 7], 10, 0));
+console.log(trimArray([1, 7], 0, 90));
