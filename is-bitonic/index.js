2f1d281abe3fedcc908efebc4efab54f4290d6ad
AmineBenaddiGitHub <36155069+AmineBenaddiGitHub@users.noreply.github.com>
2023-12-18 09:08:35 +0100

    isBitonic

---
+++ is-bitonic/index.js
@@ -0,0 +1,16 @@
+function isBitonic(arr) {
+  let max = Math.max(...arr),
+    idx = arr.indexOf(max),
+    bitonic = idx < arr.length - 1;
+  for (let i = 0; i < idx - 1 && bitonic; i++) {
+    if (arr[i] > arr[i + 1]) bitonic = false;
+  }
+  for (let i = idx; i < arr.length && bitonic; i++) {
+    if (arr[i] < arr[i + 1]) bitonic = false;
+  }
+  return { bitonic, max };
+}
+
+console.log(isBitonic([1, 2, 3, 2]));
+console.log(isBitonic([1, 2, 3]));
+console.log(isBitonic([3, 4, 5, 5, 5, 2, 1]));
