801e3c91abf762206d6413148ec297f6326f54aa
AmineBenaddiGitHub <36155069+AmineBenaddiGitHub@users.noreply.github.com>
2024-01-15 10:19:33 +0100

    Update index.js

--- matrix-flip/index.js
+++ matrix-flip/index.js
@@ -1,3 +1,22 @@
+/***
+
+Given a 2D array, write a function that flips it vertically or horizontally.
+
+Example:
+
+let array = [
+[1,2,3],
+[4,5,6],
+[7,8,9]
+]
+
+> flip(array, 'horizontal')
+> [[3,2,1],[6,5,4],[9,8,7]]
+
+> flip(array, 'vertical')
+> [[7,8,9],[4,5,6],[1,2,3]]
+***/
+
 function flip(matrix, direction) {
   switch (direction) {
     case "horizontal":
