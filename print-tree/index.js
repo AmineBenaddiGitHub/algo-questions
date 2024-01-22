e41db8f749d7d694bd659b8ac2aa9d884ce0e7f8
AmineBenaddiGitHub <36155069+AmineBenaddiGitHub@users.noreply.github.com>
2024-01-22 23:25:20 +0100

    Update print-tree

--- print-tree/index.js
+++ print-tree/index.js
@@ -1,3 +1,24 @@
+/**
+Write a data structure for a simple binary tree, and a function that prints a given tree.
+
+Example:
+
+let root = new Node(1);
+    root.left = new Node(2);
+    root.right = new Node(3);
+    root.left.left = new Node(4);
+    root.left.right = new Node(5);
+
+> printTree(root)
+> "
+    1
+   / \
+  2   3
+ / \
+4   5
+"
+*/
+
 function Node(val) {
   if (val) {
     this.val = val;
