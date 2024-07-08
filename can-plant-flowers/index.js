0d31bc1a94aedff6c4fcd1677c48f62d4328c396
AmineBenaddiGitHub <36155069+AmineBenaddiGitHub@users.noreply.github.com>
2024-07-08 16:23:16 +0100

    can-plant-flowers

---
+++ can-plant-flowers/index.js
@@ -0,0 +1,39 @@
+/*
+Write a function that takes an array of integers representing the number of flowers planted in a line,
+and an integer k representing the number of additional flowers you want to plant.
+Return whether it's possible to plant all k flowers without planting any two flowers adjacent to each other.
+*/
+
+function canPlantFlowers(arr, k) {
+  let possibilities = k;
+  arr.forEach((e, idx) => {
+    if (idx === 0 && e === 0 && idx + 1 < arr.length && arr[idx + 1] === 0) {
+      possibilities--;
+      arr[idx] = 1;
+    }
+    if (
+      idx > 0 &&
+      arr[idx - 1] === 0 &&
+      e === 0 &&
+      idx + 1 < arr.length &&
+      arr[idx + 1] === 0
+    ) {
+      possibilities--;
+      arr[idx] = 1;
+    }
+
+    if (idx > 0 && arr[idx - 1] === 0 && e === 0 && idx + 1 === arr.length) {
+      possibilities--;
+      arr[idx] = 1;
+    }
+  });
+  return possibilities <= 0;
+}
+
+console.log(canPlantFlowers([1, 0, 0, 0, 1], 1) === true);
+console.log(canPlantFlowers([1, 0, 0, 0, 1], 2) === false);
+console.log(canPlantFlowers([0, 0, 0, 0, 0], 3) === true);
+console.log(canPlantFlowers([1, 0, 1, 0, 1], 1) === false);
+console.log(canPlantFlowers([0, 0, 0], 1) === true);
+console.log(canPlantFlowers([0, 0, 0], 2) === true);
+console.log(canPlantFlowers([0, 0, 0], 3) === false);
