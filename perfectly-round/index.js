80914d3ed4f3db5bef603551600f8abe203df9b7
AmineBenaddiGitHub <36155069+AmineBenaddiGitHub@users.noreply.github.com>
2024-12-23 12:29:20 +0100

    Update index.js

--- perfectly-round/index.js
+++ perfectly-round/index.js
@@ -4,17 +4,17 @@ A cookie is "perfectly round" if its size ends with a zero.
 Write a function to determine how many perfectly round cookies will be
 made when baking with n! ingredients.
  */
-
 function countPerfectlyRoundCookies(n) {
   // The way to obtain 0: (2, 5)^n ...
-  // The number of 5's is called p-adic valuation of 5
+  // The limiting number is 5 (we have a lot of 2s)
+  // The number of 5's in the multiplication is called
+  // p-adic valuation of 5
   const maxExponent = Math.ceil(Math.log(n) / Math.log(5));
   return Array.from({ length: maxExponent }, (_, idx) => idx + 1).reduce(
     (acc, v) => acc + Math.floor(n / Math.pow(5, v)),
     0
   );
 }
-
 console.log(countPerfectlyRoundCookies(5));
 console.log(countPerfectlyRoundCookies(10));
 console.log(countPerfectlyRoundCookies(100));
