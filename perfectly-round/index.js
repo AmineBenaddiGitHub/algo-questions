3d27e7a3df93b8371998d59e4ded832d52f1ac8b
AmineBenaddiGitHub <36155069+AmineBenaddiGitHub@users.noreply.github.com>
2024-12-23 12:25:41 +0100

    perfectly-round

---
+++ perfectly-round/index.js
@@ -0,0 +1,20 @@
+/**
+At the Magic Cookie Factory, cookies are baked in factorial quantities.
+A cookie is "perfectly round" if its size ends with a zero.
+Write a function to determine how many perfectly round cookies will be
+made when baking with n! ingredients.
+ */
+
+function countPerfectlyRoundCookies(n) {
+  // The way to obtain 0: (2, 5)^n ...
+  // The number of 5's is called p-adic valuation of 5
+  const maxExponent = Math.ceil(Math.log(n) / Math.log(5));
+  return Array.from({ length: maxExponent }, (_, idx) => idx + 1).reduce(
+    (acc, v) => acc + Math.floor(n / Math.pow(5, v)),
+    0
+  );
+}
+
+console.log(countPerfectlyRoundCookies(5));
+console.log(countPerfectlyRoundCookies(10));
+console.log(countPerfectlyRoundCookies(100));
