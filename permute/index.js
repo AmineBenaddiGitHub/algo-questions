5caade8843841dfa994bff3a02d9d6d38e871d8f
AmineBenaddiGitHub <36155069+AmineBenaddiGitHub@users.noreply.github.com>
2025-01-06 08:56:17 +0100

    permute

--- permute/index.js
+++ permute/index.js
@@ -1,15 +1,15 @@
 /*
 Write a function that generates all possible permutations of a given string.
 */
-function permute(str, path = [], res = []) {
-  const combination =
-    path.length > 0
-      ? path
-      : Array.from({ length: str.length }, () => undefined);
-  if (!combination.some((e) => e === undefined)) res.push(combination);
+function permute(
+  str,
+  path = Array.from({ length: str.length }, () => undefined),
+  res = []
+) {
+  if (!path.some((e) => e === undefined)) res.push(path);
   else
-    combination.forEach((_, idx) => {
-      const copyPath = structuredClone(combination);
+    path.forEach((_, idx) => {
+      const copyPath = structuredClone(path);
       if (copyPath[idx] === undefined) {
         copyPath[idx] = str[0];
         permute(str.slice(1), copyPath, res);
